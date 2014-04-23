38b0f75784a8ebd8f27f284a2388da5b0d3b7c7b
smartemwa <smartemwa@gmail.com>
2014-04-23 15:45:49 +0300

    test commit and deploy via fabric.

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,17 @@
-from fabric.api import run, local
+from fabric.api import run, local, settings, abort
 from fabric.contrib.console import confirm
 
 
 def host_type():
     local('uname -s')
 
-
-def commit_changes():	
+def commit():	
 	with settings(warn_only=True):
-		result = local("git add . && git commit -m initial fabric file added.")
+		result = local("git add . && git commit -m 'test commit and deploy via fabric.'")
 		if result.failed and not confirm("Git add and commit has failed. Continue anyway?"):
 			abort("Aborting by user request")
 
-
-def push_changes():
+def push():
 	with settings(warn_only=True):
 		result = local("git push origin develop")
 		if result.failed and not confirm("Git add and commit has failed. Continue anyway?"):
