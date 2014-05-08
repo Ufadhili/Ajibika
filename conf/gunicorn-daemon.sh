42e1aead75d16078d0f2dffbc5f10b60c2fe711f
smartemwa <smartemwa@gmail.com>
2014-05-08 12:27:46 +0300

    upstart job

--- conf/gunicorn-daemon.sh
+++ conf/gunicorn-daemon.sh
@@ -7,10 +7,11 @@
   USER=ubuntu
   GROUP=ubuntu
   cd "/var/www/ufadhili"
-  # source ../bin/activate
+  source /var/www/ufadhili/pombola-virtualenv/bin activate
   
   test -d $LOGDIR || mkdir -p $LOGDIR
-  exec /usr/local/binbin/gunicorn_django -w $NUM_WORKERS \
+  exec /usr/local/bin/gunicorn_django pombola.wsgi:application \
+     -w $NUM_WORKERS \
     --user=$USER --group=$GROUP --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE
 
