d16d1cc9932505dfa6216afadd773db791ebdf8a
smartemwa <smartemwa@gmail.com>
2014-08-12 12:35:58 +0300

    new homepage collage

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -149,13 +149,13 @@ if DEBUG:
     MEDIA_URL = '/media_root/'
 else:
     DEFAULT_FILE_STORAGE = 'pombola.settings.ajibika_s3utils.MediaRootS3BotoStorage'
-    # STATICFILES_STORAGE = 'pombola.settings.ajibika_s3utils.StaticRootS3BotoStorage'    
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATICFILES_STORAGE = 'pombola.settings.ajibika_s3utils.StaticRootS3BotoStorage'    
+    # STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://static.ajibika.org/'
-    S3_URL = 'http://static.ajibika.org.s3-website-eu-west-1.amazonaws.com/'
+    # S3_URL = 'http://static.ajibika.org.s3-website-eu-west-1.amazonaws.com/'
     # S3_URL = 'http://ajibika.s3-eu-west-1.amazonaws.com/'
-    # STATIC_URL = '%saji-static/' % S3_URL
-    STATIC_URL = S3_URL
+    STATIC_URL = '%sstatic/' % S3_URL
+    # STATIC_URL = S3_URL
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = config.get('DJANGO_SECRET_KEY')
