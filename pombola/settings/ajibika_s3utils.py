ae1de0541c3223bd03836f62ebae05f51c2b56e4
smartemwa <smartemwa@gmail.com>
2014-08-12 12:50:40 +0300

    staticfiles test

--- pombola/settings/ajibika_s3utils.py
+++ pombola/settings/ajibika_s3utils.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto import S3BotoStorage
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='aji-static')
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+# StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='aji-media')
