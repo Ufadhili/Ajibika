c7caa73237016edb99b77ca7ec512001451f8901
smartemwa <smartemwa@gmail.com>
2014-08-12 12:18:42 +0300

    updates to s3 configs

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -152,7 +152,7 @@ else:
     STATICFILES_STORAGE = 'pombola.settings.ajibika_s3utils.StaticRootS3BotoStorage'    
     # STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     # S3_URL = 'http://static.ajibika.org/'
-    S3_URL = 'http://ajibika.s3-eu-west-1.amazonaws.com/'
+    S3_URL = 'http://aji-static.s3-eu-west-1.amazonaws.com/'
     # STATIC_URL = '%saji-static/' % S3_URL
     STATIC_URL = S3_URL
 
