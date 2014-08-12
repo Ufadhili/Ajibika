a8db98bebfd7847e39b5beab9256868a56d87b89
smartemwa <smartemwa@gmail.com>
2014-08-12 13:00:43 +0300

    more changes for static files

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -155,7 +155,8 @@ else:
     # S3_URL = 'http://static.ajibika.org.s3-website-eu-west-1.amazonaws.com/'
     # S3_URL = 'http://ajibika.s3-eu-west-1.amazonaws.com/'
     # STATIC_URL = '%saji-static/' % S3_URL
-    STATIC_URL = 'http://ajibika.s3-eu-west-1.amazonaws.com/aji-static/'
+    # STATIC_URL = 'http://ajibika.s3-eu-west-1.amazonaws.com/aji-static/'
+    STATIC_URL = 'http://static.ajibika.org/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = config.get('DJANGO_SECRET_KEY')
